ece687fe3b6ad26ed2d95582dfccc31218a29b6e
Leo-Dias10 <leo.damasceno1404@gmail.com>
2025-04-25 21:16:37 -0300

    salvar login

--- tela login/script.js
+++ tela login/script.js
@@ -3,10 +3,10 @@ var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("#body");
 
-btnSignin.addEventListener("clik", function(){
+btnSignin.addEventListener("click", function(){
     body.className = "sign-in-js";
 });
 
 btnSignup.addEventListener("click", function(){
     body.className = "sign-up-js";
-})
+});
