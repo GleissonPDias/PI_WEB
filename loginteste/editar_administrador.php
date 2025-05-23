999d4fcec73ef420c7efad78b01147ed86a362ae
Renan Nunes <142848799+RenanNunesz10@users.noreply.github.com>
2025-05-23 15:16:43 -0300

    update

--- loginteste/editar_administrador.php
+++ loginteste/editar_administrador.php
@@ -1,11 +1,5 @@
 <?php
 
-//Nesse arquivo, primeiro recuperamos os dados do administrador que queremos editar (que foram enviados da página listar_administrador.php ao clicar o link editar). 
-//Para saber qual administrador estamos considerando, recuperamos via superglobal $_GET o id do administrador em questão
-//Feito isso, vamos para a página html que está no final desse arquivo e apresentamos os dados num formulário html, que nos permite editar os dados necessários.
-//Feito isso, temos no html, no final da página, um botão que permite enviar os dados para serem atualizados no BD via script php que está no meio desse arquivo aqui
-//Aí, nesse  script php, recebemos os dados que foram atualizados no formulário e enviados via método post para esse arquivo mesmo e atualizamos os dados no BD
-//
 session_start();
 
 require_once('../conexao_db.php');
@@ -16,7 +10,7 @@ if (!isset($_SESSION['admin_logado'])) {
 }
 
 //1. Recuperando o id do administrador que foi passado na página listar_administrador ao clicar o link de editar______________________________________________________
-$adm_id = $_GET['id']; //busca o id na linha <a href="editar_administrador.php?id=<?php echo $adm['ADM_ID']; ? >" class="action-btn">Editar</a>         definido em ?id=<?php echo $adm['ADM_ID']; ? >   que está no arquivo listar_administrador.php
+$adm_id = $_GET['id'];
 
 // Busca as informações do administrador no BD
 $stmt_adm = $pdo->prepare("SELECT * FROM ADMINISTRADOR WHERE ADM_ID = :adm_id");
@@ -61,11 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
 <h2>Editar Administrador</h2>
 <form action="" method="post" enctype="multipart/form-data"> 
-<!-- Atributo enctype="multipart/form-data" em um formulário HTML é usado para especificar como os dados do formulário devem ser codificados ao serem enviados para o servidor.
-Quando um formulário contém um campo de upload de arquivos (usando <input type="file">), o enctype="multipart/form-data" é obrigatório. Ele permite que o navegador envie os dados do formulário, incluindo arquivos, de maneira apropriada ao servidor.
-Aqui está o que acontece quando você usa o enctype="multipart/form-data":
--Envio de Arquivos: O método padrão de envio de formulários (application/x-www-form-urlencoded) não é adequado para o envio de arquivos, pois ele codifica os dados em um formato de URL, o que não suporta a transmissão de arquivos binários. O multipart/form-data permite que arquivos sejam enviados corretamente, dividindo os dados em partes que incluem o conteúdo do arquivo.
--Segmentação de Campos: Os dados do formulário são divididos em várias partes, e cada campo é tratado como uma parte separada da solicitação HTTP, permitindo que tanto dados de texto quanto arquivos sejam enviados no mesmo formulário. -->
+
     <label for="nome">Nome:</label>
     <input type="text" name="nome" id="nome" value="<?= $adm['ADM_NOME'] ?>" required>
     <p>
@@ -77,10 +67,6 @@ Aqui está o que acontece quando você usa o enctype="multipart/form-data":
     <label for="ativo">Ativo:</label>
     <input type="checkbox" name="ativo" id="ativo" value="1" <?= $adm['ADM_ATIVO'] ? 'checked' : '' ?>>
     <p>
-    <!-- 
-    value="1"   Define o que será enviado ao servidor, se o checkbox estiver marcado
-    checked     Controla o estado inicial do checkbox quando a página carrega (se vai aparecer marcado ou não). Sem ele, mesmo que value="1", o checkbox inicia sempre desmarcado. --> 
-
 
     <p>
     <button type="submit">Atualizar Administrador</button>
